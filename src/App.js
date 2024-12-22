4196387339d4ce17de4228d8dbdc1751eadbd5c6
Chiranjeevi Allem <chiranjeeviallem@Chiranjeevis-MacBook-Air.local>
2024-12-22 20:21:20 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter } from "react-router-dom";
+import Body from "./components/Body";
+
+import Browse from "./components/Browse";
+import { RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
---
+++ src/components/Browse.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Browse() {
+  return <div>Browse</div>;
+}
+export default Browse;
